e03f4e89e710d5afcf4fde5bbf4fb9f4819ef49c
Michiel Meilink <michielmeilink@gmail.com>
2017-09-26 20:43:17 +0200

    Laravel | Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-use App\Task;
 
+Route::get('/tasks', 'TasksController@index');
+Route::get('tasks/{index}', 'TasksController@show');
 
-Route::get('/tasks', function () {
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('tasks/{task}', function ($id) {
-    $task = Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
