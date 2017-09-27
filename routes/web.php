49296ecd07519245d1c7a3c71810bfb81eeb1985
Michiel Meilink <michielmeilink@gmail.com>
2017-09-27 12:14:56 +0200

    Laravel | Setting up a new project for making a website on my own

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
-Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
